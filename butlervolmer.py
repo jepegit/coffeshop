f115911f33bc059823e080bb346fe8a0d66c5961
juliawind <julia.wind@ife.no>
2020-03-05 14:09:37 +0100

    update butlervolmer.py

--- butlervolmer.py
+++ butlervolmer.py
@@ -1,4 +1,26 @@
 #
 # This will eventually solve the BV equation
 #
-print("Guten Morgen!")
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+R=8.3145 # Gas constant
+F=96485 # Faraday constant
+alpha=0.5 # charge transfer coeff
+z=1 # nr of electrons
+j0=1 # exchange current density
+T=300 # Temperature
+
+eta=np.linspace(-0.1,0.1,100) # overpotential
+
+ja=j0*np.exp(alpha*z*F/(R*T)*eta)       # anodic current density
+jn=-j0*np.exp(-(1-alpha)*z*F/(R*T)*eta) # cathodic current density
+j=ja+jn                                 # total current density
+plt.plot(eta,ja)
+plt.plot(eta,jn)
+plt.plot(eta,j)
+plt.xlabel('Overpotential [V]')
+plt.ylabel('Current Density [A/m2]')
+plt.legend(['ja', 'jn', 'j'], loc='upper left')
+plt.show()
