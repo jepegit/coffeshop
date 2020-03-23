058a492284600475974779927cefdc31a7963e42
Vilde Stueland Nysted <vilde.nysted@haugnett.no>
2020-03-23 10:43:45 +0100

    Add simple plotting example in make_plot.py

--- make_plot.py
+++ make_plot.py
@@ -1,1 +1,19 @@
-print("Hello World!")
+import matplotlib.pyplot as plt
+import numpy as np
+
+#%% Simple plotting example
+
+# Create some data to plot
+x = np.linspace(-10, 10, 50)
+y = x**2
+
+# Plot the data
+plt.figure()
+plt.plot(x, y, 'r')  # The last letter determines the colour
+plt.xlabel('x')
+plt.ylabel(r'x$^2$')  # Can write latex formula by using $
+plt.title(r'Plot of x$^2$')
+plt.show()
+
+#%% Plotting voltage vs time with cellpy
+
